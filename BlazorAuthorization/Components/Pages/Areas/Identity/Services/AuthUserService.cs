e4dcaefa00633933aa5cb8d37cd97abb242fb27d
Zrinko Vukšić <zrinko.vuksic@king-ict.cloud>
2024-04-23 11:25:43 +0200

    Added GetData endpoint

--- BlazorAuthorization/Components/Pages/Areas/Identity/Services/AuthUserService.cs
+++ BlazorAuthorization/Components/Pages/Areas/Identity/Services/AuthUserService.cs
@@ -2,6 +2,7 @@
 using BlazorAuthorization.SharedServices;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace BlazorAuthorization.Components.Pages.Areas.Identity.Services
 {
@@ -11,22 +12,6 @@ namespace BlazorAuthorization.Components.Pages.Areas.Identity.Services
         {
         }
 
-        //public User? LookupUserInDatabase(string username, string password)
-        //{
-        //    var usersFromDatabase = new List<User>()
-        //    {
-        //        new()
-        //        {
-        //            Username = "user",
-        //            Password = "blazorschool"
-        //        }
-        //    };
-
-        //    var foundUser = usersFromDatabase.SingleOrDefault(u => u.Username == username && u.Password == password);
-
-        //    return foundUser;
-        //}
-
         public async Task<AuthResponse?> Login(User request)
         {
             var result = await _httpClient.PostAsJsonAsync("Login/Login", request);
@@ -34,6 +19,24 @@ namespace BlazorAuthorization.Components.Pages.Areas.Identity.Services
             {
                 var content = await result.Content.ReadAsStringAsync();
                 return JsonConvert.DeserializeObject<AuthResponse>(content);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> GetData()
+        {
+            string token = await this.GetSessionTokenToBrowserAsync();
+            if (!String.IsNullOrEmpty(token))
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var result = await _httpClient.GetAsync("Login/GetData");
+            if (result.IsSuccessStatusCode)
+            {
+                var content = await result.Content.ReadAsStringAsync();
+                //return JsonConvert.DeserializeObject<AuthResponse>(content);
+                return content;
             }
             else
             {
